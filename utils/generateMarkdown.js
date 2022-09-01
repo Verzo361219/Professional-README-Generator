db3eb435778db452f1878c0324b006a0f07b4d8f
Michael Hobbs <michaelhobbs361219@gmail.com>
2022-09-01 18:12:08 -0400

    added function to generalmarkdown.js to add a statement if the user does not want contributers

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -47,6 +47,19 @@ function renderLicenseTOC(license) {
     return ' ';
   }
  }
+ 
+// Function to return a message if user doesn't want contributors
+function renderContributingSection(confirmContributers, data) {
+  if (!confirmContributers) {
+    return `
+  Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+    `;
+  } else {
+    return `
+  ${data}
+    `;
+  }
+}
 
  // Function to generate markdown for README
 function generateMarkdown(data) {
